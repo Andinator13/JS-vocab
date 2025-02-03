2f6d340f5feb9b35a55a1b2cc302cab928be8c77
Good_Old_Andy <andvyster@gmail.com>
2025-02-03 14:21:51 +0200

    beginnings of my js vocab-notes

---
+++ js/basics.js
@@ -0,0 +1,17 @@
+'use strict'; // Код у суворому режимі
+
+// 1. Виведеня даних
+
+// Метод console.log()
+
+console.log("JavaScript is awesome!");
+console.log(10);
+
+// 2. Змінні та типи даних
+
+//Оголошення даних
+
+const age = 20; 
+const username = "Mango";
+
+
