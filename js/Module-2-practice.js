e03209c2337a08b4e1f3a03d2157bab179474d9a
Good_Old_Andy <andvyster@gmail.com>
2025-02-07 17:04:12 +0200

    updaate Module-2.js and Module-2-practice.js

--- js/Module-2-practice.js
+++ js/Module-2-practice.js
@@ -2,17 +2,54 @@
  * 
  */
 
-let count = 13;
+// let count = 0;
 
-count = 3;
-
-while (count < 21) {
+// while (count <= 5) {
   
-    count **= 2;
+//     count += 1;
     
-    console.log(`Tell me why: ${count}`);
-}
+//     console.log(`Count: ${count}`);
+// }
 
 // console.log(`Count: ${count}`);
 
 
+// let clientCounter = 1;
+
+// const maxClients = 50;
+
+// while (clientCounter < maxClients) {
+//     clientCounter += 1;
+
+//     console.log(clientCounter);
+// }
+
+// function countClients(clientCounter, maxClients) {
+// //   let counter = clientCounter; // створюємо локальну змінну
+
+//   while (clientCounter < maxClients) {
+//     console.log(clientCounter);
+//     clientCounter += 1;
+//   }
+// }
+
+// countClients(18, 25);
+
+
+// let count = 0;
+
+// do {
+// 	console.log(`Count: ${count}`);
+// 	count += 1;
+// } while (count <= 5);
+
+// console.log(`Count: ${count}`);
+
+// let count = 0;
+
+// do {
+// 	    count += 1;
+//         console.log(`Count: ${count}`);
+// } while (count <= 5);
+
+// console.log(`Count: ${count}`);
--- js/Module-2.js
+++ js/Module-2.js
@@ -2,15 +2,51 @@
  * 
  */
 
-let count = 0;
+// let count = 0;
 
-while (count < 10) {
+// while (count < 10) {
   
-    count **= 2;
+//     count += 2;
     
-    console.log(`Tell me why: ${count}`);
-}
+//     console.log(`Tell me why: ${count}`);
+// }
 
 // console.log(`Count: ${count}`);
 
+
+/** Задача: Реєстрація у готелі */
+
+// //Змінна clientCounter зберігає кількість зайнятих номерів на поточний момент.
+
+// Змінна maxClients зберігає загальну кількість номерів у готелі.Завдяки циклу while місця в готелі будуть заповнюватися доти, доки поточна кількість клієнтів не дорівнюватиме максимально допустимій.
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// function countClients(clientCounter, maxClients) {
+//   let counter = clientCounter; // створюємо локальну змінну
+
+//   while (counter < maxClients) {
+//     console.log(counter);
+//     counter += 1;
+//   }
+// }
+
+// countClients(18, 25);
+
+/** Цикл do…while */
+
+// let count = 0;
+
+// do {
+// 	console.log(`Count: ${count}`);
+// 	count += 1;
+// } while (count < 5);
+
+/** Цикл for */
 
