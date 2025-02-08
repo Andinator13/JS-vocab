01214328dfd4e6dd65c04e4f65ae16b0c20eb15f
Good_Old_Andy <andvyster@gmail.com>
2025-02-08 18:40:07 +0200

    update Module-1-practice.js

---
+++ js/Module-1-practice.js
@@ -0,0 +1,23 @@
+// const message = 'Hello, world!'
+
+// console.log(message.length);
+
+// const title = 'Beauty and the Beast';
+
+// const length = title.length;
+
+// console.log(length);
+
+// console.log(title[15]);
+
+// const index = title.length - 1;
+
+// console.log(title[index]);
+
+// const companyName = 'Boston Dynamics';
+// const repairBots = 150;
+// const defenceBots = 50;
+
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`
+
+// console.log(message); 
