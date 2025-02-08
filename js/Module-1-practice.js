dd3366059a2042f0173243f0ae9bf600924a0fa1
Good_Old_Andy <andvyster@gmail.com>
2025-02-08 19:46:24 +0200

    update Module-1-practice.js

--- js/Module-1-practice.js
+++ js/Module-1-practice.js
@@ -20,4 +20,48 @@
 
 // const message = `${companyName} has ${repairBots + defenceBots} bots in stock`
 
-// console.log(message); 
+// console.log(message);
+
+/** Перетворення типів: рядки */
+
+// console.log(5);
+// console.log(String(5));
+
+// const str = String(5);
+
+// console.log(str);
+
+// console.log(typeof str);
+
+// function cool (x) {
+//     const value = 5;
+
+//     return value
+// }
+
+// console.log(x);
+
+/** Перетворення типів: number */
+
+// console.log(Number("132"));
+
+// console.log(5 + true + true);
+// console.log("6" * 5);
+// console.log(14 - "1");
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("25px"));
+// console.log(typeof Number("25px"));
+// console.log(Number(null));
+// console.log(Number(undefined));
+
+/** Оператори порівняння */
+
+console.log(13 > 7);
+console.log(13 > "7");
+console.log(13 > "7");
+console.log(13 >= "13");
+// console.log(13 > 14);
+// console.log(14 > 13);
+// console.log(13 > 14);
