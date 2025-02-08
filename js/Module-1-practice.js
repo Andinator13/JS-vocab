47cd1fa438ed1e610985f2db1f76b7599678fd20
Good_Old_Andy <andvyster@gmail.com>
2025-02-08 20:33:24 +0200

    update Module-1-practice.js

--- js/Module-1-practice.js
+++ js/Module-1-practice.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // const message = 'Hello, world!'
 
 // console.log(message.length);
@@ -56,12 +58,70 @@
 // console.log(Number(null));
 // console.log(Number(undefined));
 
-/** Оператори порівняння */
+/** Оператори 
+ * - порівняння
+ * - рівності
+ * - приведення типів операндів
+ */
 
-console.log(13 > 7);
-console.log(13 > "7");
-console.log(13 > "7");
-console.log(13 >= "13");
+// console.log(13 > 7);
+// console.log(13 > "7");
+// console.log(13 > "7");
+// console.log(13 >= "13");
 // console.log(13 > 14);
 // console.log(14 > 13);
 // console.log(13 > 14);
+
+// console.log("4" == 4);
+// console.log("4" === 4);
+// console.log("4" === "4");
+
+// console.log(undefined == null);
+
+/** 
+ * Арифметичні функції
+ * 
+ * - Number.parseInt
+ * - Number.paarseFloat
+ * - Math
+ */
+
+// let width = "375px";
+
+// width = Number(width);
+
+// width = Number.parseInt(width);
+
+// let height = "205.5px";
+
+// height = Number.parseFloat(height);
+
+// console.log(width);
+// console.log(height);
+
+// const age = prompt("А скільки вам рочків?");
+
+// if (age >= 18) {
+//     console.log("Все чудово! Можете дивитись порно :)");
+    
+// } else {
+//     console.log("Опа, приїхали. СБУ вже слідкує за тобою! А ну виходь, малеча!!!");
+// }
+
+// let num = 13.1;
+
+// num = 13.7
+
+// // console.log(Math.floor(num));
+// // console.log(Math.ceil(num));
+
+// // const word = "floor";
+// // console.log(word.length);
+// console.log("floor".length);
+// console.log("ceiling".length);
+
+let num = 14.3
+console.log(Math.round(num));
+
+num = 14.7
+console.log(Math.round(num));
