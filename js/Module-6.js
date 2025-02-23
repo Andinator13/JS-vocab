7e9b0523ecd78f7c73d1a04a1adb70b64044a5d9
Good_Old_Andy <andvyster@gmail.com>
2025-02-24 01:43:19 +0200

    update Module-6.js

---
+++ js/Module-6.js
@@ -0,0 +1,13 @@
+/** Контекст виконання функції */
+
+// Ключове слово this
+
+const user = {
+  username: "Victor",
+  showName() {
+		// ✅ Віктор біжить швидко, тому що він (this) намагається зловити поїзд.
+    console.log(this.username);
+  },
+};
+
+user.showName();
