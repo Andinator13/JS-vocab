4fe919917fa98e676fada6e9e5299da87d6899b3
Good_Old_Andy <andvyster@gmail.com>
2025-02-23 22:36:40 +0200

    update Module-5.js and Module-5-practice.js

--- js/Module-5-practice.js
+++ js/Module-5-practice.js
@@ -99,11 +99,115 @@
 
 // console.log(arrowFoo(10, 20));
 
-function greet(name) {
-  console.log(`Hello, ${name}! I am ${this.person}`);
+// function greet(name) {
+//   console.log(`Hello, ${name}! I am ${this.person}`);
+// }
+// const person = "John";
+// const context = {
+//   person: "Alice",
+// };
+// greet.call(context, "Bob");
+
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach(square)
+// numbers.forEach(double);
+// numbers.forEach(display);
+
+// function double(element, index, array) {
+//   array[index] = element * 2;
+// }
+
+// function square(element, index, array) {
+//   array[index] = Math.pow(element, 2);
+// }
+
+// function display(element) {
+// console.log(element);
+
+// }
+
+// let fruits = ["apple", "orange", "banana", "coconut"];
+
+// fruits.forEach(capitalize)
+// fruits.forEach(display);
+
+// function capitalize(element, index, array) {
+//   array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+// }
+
+// function lowerCase(element, index, array) {
+//   array[index] = element.toLowerCase();
+// }
+
+// function display(element) {
+// console.log(element);
+// }
+
+
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   const sum = (item) => {
+//     return totalPrice += item;
+//   }
+
+//   orderedItems.forEach(sum);
+
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   const addNumber = (number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   };
+
+//   numbers.forEach(addNumber);
+
+//   return filteredNumbers;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// function changeEven(numbers, value) {
+
+//   const newArray = [];
+
+//   numbers.forEach(element => {
+//        newArray.push(element % 2 === 0 ? element + value : element)
+//   });
+
+      
+//   return newArray;
+// };
+
+// const array = [1, 2, 3, 4, 5];
+// const doubledNumbers = changeEven(array, 2);
+
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+// console.log(changeEven([17, 24, 68, 31, 42], 100));
+// console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+
+function showPureFunctionLoad() {
+  
 }
-const person = "John";
-const context = {
-  person: "Alice",
-};
-greet.call(context, "Bob");
+
+
+
+
--- js/Module-5.js
+++ js/Module-5.js
@@ -1,20 +1,120 @@
 /** Колбек-функції */
 
-function greet(name) {
-  console.log(`Welcome ${name}!`);
+// function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// }
+
+// function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+
+// registerGuest("Mango", greet); // "Registering Mango!"
+// 															 // "Welcome Mango!"
+
+// registerGuest("Mango", notify); // "Registering Mango!"
+// 																// "Dear Mango, your room will be ready in 30 minutes"
+
+/** Стрілочні функції */
+
+// Звичайне оголошення функції
+function classicAdd(a, b, c) {
+  return a + b + c;
 }
 
-function notify(name) {
-  console.log(`Dear ${name}, your room will be ready in 30 minutes`);
-}
+// Те саме стрілочною функцією
+const arrowAdd = (a, b, c) => {
+  return a + b + c;
+};
 
-function registerGuest(name, callback) {
-  console.log(`Registering ${name}!`);
-  callback(name);
-}
+const add = a => {
+  return a + 5;
+};
 
-registerGuest("Mango", greet); // "Registering Mango!"
-															 // "Welcome Mango!"
+const greet = () => {
+  console.log('Hello!');
+};
 
-registerGuest("Mango", notify); // "Registering Mango!"
-																// "Dear Mango, your room will be ready in 30 minutes"
+// Явне повернення (explicit return)
+
+const addEx = (a, b, c) => {
+  console.log(a, b, c);
+  return a + b + c;
+};
+
+// Неявне повернення (implicit return)
+
+const addImp = (a, b, c) => a + b + c;
+
+// ﻿Псевдомасив arguments
+
+// Якщо необхідно зібрати всі аргументи в масив, використовується операція rest.
+
+const addArgs = (...args) => {
+  console.log(args);
+};
+
+addArgs(1, 2, 3); // [1, 2, 3]
+
+// Колбеки
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// Звичайна анонімна функція
+// numbers.forEach(function (number, index) {
+//   console.log(`Index ${index}, value ${number}`);
+// });
+
+// // Стрілочна анонімна функція
+// numbers.forEach((number, index) => {
+//   console.log(`Index ${index}, value ${number}`);
+// });
+
+// Стрілочну колбек-функцію також можна оголошувати окремо й передавати на неї посилання
+
+// const logMessage = (number, index) => {
+//   console.log(`Index ${index}, value ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+/** Методи map і flatMap */
+
+// Чисті функції
+
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// dirtyMultiply(numbers, 2);
+// // Відбулася мутація вихідних даних - масиву numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers1, 2);
+
+// // Мутація вихідних даних не відбулася
+// console.log(numbers1); // [1, 2, 3, 4, 5]
+// // Функція повернула новий масив зі зміненими даними
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+
+// Перебираючі методи
+
