93b0f4e6f48d45dc0bb364f93f4a795d1c64137e
Good_Old_Andy <andvyster@gmail.com>
2025-02-26 16:42:41 +0200

    create Methods.js and Objects.js

---
+++ js/Methods.js
@@ -0,0 +1,75 @@
+// let userName = "Peter";
+
+// console.log(userName);
+
+// let loudSpeaker = userName.toUpperCase();
+
+// console.log(loudSpeaker);
+
+// const message = `And this is when he yelled, ${loudSpeaker}!!!`
+
+// console.log(message);
+
+// function getBlockLetters(name) {
+//     return name.toUpperCase()
+// }
+
+// console.log(getBlockLetters("Jason"));
+
+/** Exercise 1 */
+
+let city = "Amsterdam";
+console.log(city);
+
+city = "New York";
+console.log(city);
+
+/** Exercise 2 */
+
+function getNameLength(name) {
+    return name.length;
+}
+
+console.log(getNameLength("John"));
+;
+
+/** Exercise 3 */
+
+function double(number) {
+    return number * 2;
+}
+
+console.log(double(13));
+
+
+/** Exercise 4 */
+function getNextCentury(year) {
+    // console.log(year + 100);
+    return year + 100;
+}
+
+console.log(getNextCentury(50));
+console.log(getNextCentury(100));
+
+/** Exercise 5 */
+
+let name = "alex";
+let shouting = name.toUpperCase();
+
+console.log(name);
+console.log(shouting);
+
+
+
+/** Exercise 6 */
+
+let userName = "SaM";
+let lowercased = userName.toLowerCase();
+
+console.log(userName);
+console.log(lowercased);
+
+
+
+
+
---
+++ js/Objects.js
@@ -0,0 +1,28 @@
+const books = {
+  Green1: {
+    title: 'Looking for Alaska',
+    author: 'John Green',
+    year: 2013,
+    cover: 'paperback',
+    pages: 272,
+    price: 334,
+  },
+  Green2: {
+    title: 'Abundance of Katherines',
+    author: 'John Green',
+    year: 2013,
+    cover: 'paperback',
+    pages: 272,
+    price: 337,
+  },
+};
+
+console.log(books);
+
+let booklength = books.Green2.pages;
+console.log(booklength);
+let author = books.Green1.author;
+console.log(author);
+
+
+
