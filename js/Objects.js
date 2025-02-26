83aed4e6c52fac4a36aee20ad9f9a7932c051042
Good_Old_Andy <andvyster@gmail.com>
2025-02-26 16:56:12 +0200

    update Objects.js

--- js/Objects.js
+++ js/Objects.js
@@ -5,7 +5,8 @@ const books = {
     year: 2013,
     cover: 'paperback',
     pages: 272,
-    price: 334,
+        price: 334,
+    fiction: true,
   },
   Green2: {
     title: 'Abundance of Katherines',
@@ -13,16 +14,34 @@ const books = {
     year: 2013,
     cover: 'paperback',
     pages: 272,
-    price: 337,
-  },
+      price: 337,
+    fiction: true,
+    },
+  platforms: ["Twitter", "Instagram", "Facebook"]
 };
 
+// console.log(books);
+
+books.Green1.year = 2005;
 console.log(books);
 
-let booklength = books.Green2.pages;
-console.log(booklength);
-let author = books.Green1.author;
-console.log(author);
 
+// console.log(books.platforms);
+// console.log(books.platforms[2]);
+// console.log(books.platforms.length);
+// console.log(books["platforms"]);
+
+
+
+
+
+// console.log(books);
+
+// let booklength = books.Green2.pages;
+// console.log(booklength);
+// let author = books.Green1.author;
+// console.log(author);
+
+// console.log(books.Green1);
 
 
