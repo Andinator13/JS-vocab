6680ef7cb5b2575bf3b22fe6d35704555096887f
Good_Old_Andy <andvyster@gmail.com>
2025-03-01 21:01:52 +0200

    update Experiments.js

--- js/Experiments.js
+++ js/Experiments.js
@@ -1,6 +1,21 @@
-first_name = "Stacy";
-last_name = "Lovelace";
+// first_name = "Stacy";
+// last_name = "Lovelace";
 
-full_name = first_name + " " + last_name;
+// full_name = first_name + " " + last_name;
 
-console.log(full_name);
+// console.log(full_name);
+
+
+firstName = "Bruce"
+lastName = "Wayne"
+title = "Hero"
+
+greeting = "Hello, " + firstName + " " + lastName + ". Welcome, " + title + "."
+
+// interGreeting = f"Hello, {firstName} {lastName}. Welcome, {title}."
+
+console.log(greeting);
+
+console.log(title.length);
+
+
